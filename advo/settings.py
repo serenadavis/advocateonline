62a91604c1a8fe63866a857cc33b2316ef5c05a9
moue <hou@college.harvard.edu>
2014-10-18 15:55:09 -0400

    Removed djstripe

--- advo/settings.py
+++ advo/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = (
     'ajax_select',
     'south',
     'magazine',
-    'djstripe'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -102,7 +101,6 @@ STATICFILES_FINDERS = (
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.request',
-    'djstripe.context_processors.djstripe_settings',
 )
 
 AJAX_LOOKUP_CHANNELS = {
@@ -112,24 +110,6 @@ AJAX_LOOKUP_CHANNELS = {
 
 STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY", "pk_test_7mkjcG8fQj3qmdhCgP92Pq4g")
 
-DJSTRIPE_PLANS = {
-    "monthly": {
-        "stripe_plan_id": "pro-monthly",
-        "name": "Web App Pro ($24.99/month)",
-        "description": "The monthly subscription plan to WebApp",
-        "price": 2499,  # $24.99
-        "currency": "usd",
-        "interval": "month"
-    },
-    "yearly": {
-        "stripe_plan_id": "pro-yearly",
-        "name": "Web App Pro ($199/year)",
-        "description": "The annual subscription plan to WebApp",
-        "price": 19900,  # $199.00
-        "currency": "usd",
-        "interval": "year"
-    }
-}
 
 try:
     from local_settings import *
