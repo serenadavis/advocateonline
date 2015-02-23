955227a718a3f74ae47672497538e30cb288a94f
Diane Yang <dianeyang0@gmail.com>
2015-02-22 21:09:06 -0500

    set up start bootstrap admin 2 theme

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render_to_response
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello, world.")
+	return render_to_response('contacts/index.html')
