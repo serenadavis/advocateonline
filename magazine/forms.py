4293530efecefc7cdaa721c3ecffbf5010017d4f
Alexander Sedlack <asedlack@college.harvard.edu>
2015-03-02 15:26:43 -0500

    Apache Lucene implemented and search functioning with rough styling

--- magazine/forms.py
+++ magazine/forms.py
@@ -4,6 +4,7 @@ from ajax_select import make_ajax_field
 
 from .models import Content, Tag
 
+from haystack.forms import SearchForm
 
 class ContentForm(ModelForm):
     class Meta:
@@ -13,3 +14,18 @@ class ContentForm(ModelForm):
     tags = make_ajax_field(Content, 'tags', 'tag')
 
 
+
+class ContentSearchForm(SearchForm):
+
+    def no_query_found(self):
+        return self.searchqueryset.all()
+
+    def search(self):
+        sqs = super(ContentSearchForm, self).search()
+
+        if not self.is_valid():
+            return self.no_query_found()
+
+        sqs = sqs.order_by(title)
+
+        return sqs
--- magazine/search_indexes.py
+++ magazine/search_indexes.py
@@ -4,6 +4,7 @@ from .models import Content
 class ContentIndex(indexes.SearchIndex, indexes.Indexable):
     title = indexes.CharField(model_attr='title')
     
+
     text = indexes.CharField(document=True, use_template=True)
 
     def get_model(self):
