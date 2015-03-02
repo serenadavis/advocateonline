82eee5866fb998b1d911e8668959361abbe52f85
Diane Yang <dianeyang0@gmail.com>
2015-03-02 00:22:36 -0500

    started working with grappelli after realizing it had a lot of built in functionality

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
+
+class ContactAdmin(admin.ModelAdmin):
+	list_display = ('firstName', 'lastName', 'graduationYear',
+					'otherDegrees', 'profession', 'board',
+					'positionHeld', 'streetAddress1', 'city',
+					'state', 'zipCode', 'email1', 'dateAdded')
+	list_filter = ['state', 'graduationYear', 'tier', 'article', 'followed']
+	search_fields = ['firstName', 'middleName', 'lastName', 'nickName']
 
 # Register your models here.
+admin.site.register(Contact, ContactAdmin)
--- contacts/models.py
+++ contacts/models.py
@@ -2,23 +2,47 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
+	def __unicode__(self):
+		return self.article + " " + self.firstName + " " + self.lastName + " " + self.title
+
+	def fullName(self):
+		return self.article + " " + self.firstName + " " + self.lastName + " " + self.title
+
 	firstName = models.CharField(max_length=255) 
-	lastName = models.CharField(max_length=255)
+	lastName = models.CharField(max_length=255, blank=True)
 	middleName = models.CharField(max_length=255, blank=True)
-	title = models.CharField(max_length=255)
+	article = models.CharField(max_length=255, blank=True)
+	title = models.CharField(max_length=255, blank=True)
 	nickName = models.CharField(max_length=255, blank=True)
 	streetAddress1 = models.CharField(max_length=255)
 	streetAddress2 = models.CharField(max_length=255, blank=True)
+	streetAddress3 = models.CharField(max_length=255, blank=True)
 	city = models.CharField(max_length=255)
 	state = models.CharField(max_length=255)
 	zipCode = models.CharField(max_length=255)
-	email = models.CharField(max_length=255)
+	country = models.CharField(max_length=255, blank=True)
+	email1 = models.CharField(max_length=255, blank=True)
+	email2 = models.CharField(max_length=255, blank=True)
+	phone = models.CharField(max_length=255, blank=True)
+	linkedIn = models.CharField(max_length=255, blank=True)
+	twitter = models.CharField(max_length=255, blank=True)
+	facebook = models.CharField(max_length=255, blank=True)
+	followed = models.BooleanField(default=False)
+	website = models.CharField(max_length=255, blank=True)
 	profession = models.CharField(max_length=255, blank=True)
-	graduationYear = models.CharField(max_length=255)
-	degrees = models.CharField(max_length=255, blank=True)
+	graduationYear = models.CharField(max_length=255, blank=True)
+	otherDegrees = models.CharField(max_length=255, blank=True)
 	board = models.CharField(max_length=255, blank=True)
 	positionHeld = models.CharField(max_length=255, blank=True)
 	publishedWork = models.TextField(blank=True)
+	notes = models.TextField(blank=True)
+	donationBracket = models.CharField(max_length=255, blank=True)
+	#donationYears = 
+	#donatedAmount = 
+	#lastContact = 
+	tier = models.CharField(max_length=255, blank=True)
+	formCategory = models.CharField(max_length=255, blank=True)
+	dateAdded = models.DateField(auto_now_add=True, blank=True)
 
 
 class Interaction(models.Model):
