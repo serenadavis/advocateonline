4f4aaa55f783a31956bc1bea1c9a197ea4bc0e7e
Diane Yang <dianeyang0@gmail.com>
2015-03-07 19:19:22 -0500

    added ability to export to XLS spreadsheet

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,9 +1,101 @@
+import xlwt
 from django.contrib import admin
+from django.http import HttpResponse
+from django.utils.encoding import smart_str
 from .models import *
 
 class InteractionInline(admin.TabularInline):
 	model = Interaction
 	extra = 0
+
+def export_xls(modeladmin, request, queryset):
+    response = HttpResponse(mimetype='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename=contacts.xls'
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet("Contacts")
+    
+    row_num = 0
+    
+    columns = [
+        (u"Graduation year", 256*8),
+        (u"Other degrees", 256*12),
+        (u"Last name", 256*20),
+        (u"First name", 256*20),
+        (u"Middle name", 256*20),
+        (u"Article", 256*10),
+        (u"Title", 256*10),
+        (u"Nick name", 256*20),
+        (u"Address 1", 256*30),
+        (u"Address 2", 256*30),
+        (u"Address 3", 256*30),
+        (u"City", 256*15),
+        (u"State", 256*10),
+        (u"Zip code", 256*12),
+        (u"Country", 256*10),
+        (u"Email 1", 256*30),
+        (u"Email 2", 256*30),
+        (u"Phone", 256*15),
+        (u"Profession", 256*20),
+        (u"Board", 256*20),
+        (u"Position held", 256*20),
+        (u"LinkedIn", 256*30),
+        (u"Facebook", 256*30),
+        (u"Website", 256*30),
+        (u"Published work?", 256*30),
+        (u"Category", 256*15),
+        (u"Tier", 256*15),
+        (u"Notes", 256*30)
+    ]
+
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+    for col_num in xrange(len(columns)):
+        ws.write(row_num, col_num, columns[col_num][0], font_style)
+        # set column width
+        ws.col(col_num).width = columns[col_num][1]
+
+    font_style = xlwt.XFStyle()
+    font_style.alignment.wrap = 1
+    
+    for obj in queryset:
+        row_num += 1
+        row = [
+            obj.graduationYear,
+            obj.otherDegrees,
+            obj.lastName,
+            obj.firstName,
+            obj.middleName,
+            obj.article,
+            obj.title,
+            obj.nickName,
+            obj.streetAddress1,
+            obj.streetAddress2,
+            obj.streetAddress3,
+            obj.city,
+            obj.state,
+            obj.zipCode,
+            obj.country,
+            obj.email1,
+            obj.email2,
+            obj.phone,
+            obj.profession,
+            obj.board,
+            obj.positionHeld,
+            obj.linkedIn,
+            obj.facebook,
+            obj.website,
+            obj.publishedWork,
+            obj.formCategory,
+            obj.tier,
+            obj.notes
+        ]
+        for col_num in xrange(len(row)):
+            ws.write(row_num, col_num, row[col_num], font_style)
+            
+    wb.save(response)
+    return response
+export_xls.short_description = u"Export Excel spreadsheet"
 
 class ContactAdmin(admin.ModelAdmin):
 	list_display = ('firstName', 'lastName', 'graduationYear',
@@ -13,6 +105,7 @@ class ContactAdmin(admin.ModelAdmin):
 	list_filter = ['state', 'graduationYear', 'board', 'positionHeld', 'tier', 'article', 'followed', 'formCategory']
 	search_fields = ['firstName', 'middleName', 'lastName', 'nickName']
 	inlines = [InteractionInline]
+	actions = [export_xls]
 
 class InteractionAdmin(admin.ModelAdmin):
 	list_filter = ['category']
