b1b38eff55dee716cccf27b26ff8314088e46760
Alexander Sedlack <asedlack@college.harvard.edu>
2015-03-22 12:57:10 -0400

    Reverted to inline typeahead js

--- magazine/templatetags/myTags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-def typeaheadJsView(request, typeahead_authors, typeahead_titles):
-    jsContext = {'typeahead_authors' : typeahead_authors, 'typeahead_titles' : typeahead_titles}
-    return jsContext
-
-register = template.Library()
-register.inclusion_tag('jsTypeahead.js', takes_context=True)(typeaheadJsView)
