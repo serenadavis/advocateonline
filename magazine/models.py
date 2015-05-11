d6ca6930195a05bfaef854ae6a339b2cdf533a22
Yuqi Hou <yuqi.hou@gmail.com>
2015-05-11 10:23:16 -0400

    Body not required and updated requirements file

--- magazine/models.py
+++ magazine/models.py
@@ -94,7 +94,7 @@ class Content(models.Model):
     subtitle = models.CharField(max_length=255, blank=True)
     slug = models.SlugField(max_length=100, unique=True)
     teaser = tinymce_models.HTMLField(blank=True)
-    body = tinymce_models.HTMLField()
+    body = tinymce_models.HTMLField(blank=True)
 
     # Legacy fields; we should probably get rid of this eventually
     medium = tinymce_models.HTMLField(blank=True)
