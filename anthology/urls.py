dbf671cd5c2098fd8356125b237cca01e47b7880
Alexander Goldberg <alexandergoldberg@Alexanders-MacBook-Pro-2.local>
2016-01-20 12:15:01 -0500

    added urls

---
+++ anthology/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', "anthology.views.main"),
+)
