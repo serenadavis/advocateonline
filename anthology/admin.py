109a651872ca332187662d4bd37cfde3a1611166
Brendan Bozorgmir <bbozorgmir@college.harvard.edu>
2016-01-20 20:21:43 -0500

    anthology models and admin update

--- anthology/admin.py
+++ anthology/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from models import *
+from mce_filebrowser.admin import MCEFilebrowserAdmin
+
+
+class ContentAdmin(MCEFilebrowserAdmin):
+    search_fields = ['tags']
 
 # Register your models here.
 admin.site.register(Decade)
+admin.site.register(Content, ContentAdmin)
+admin.site.register(Images)
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Author)
+admin.site.register(Theme)
--- anthology/models.py
+++ anthology/models.py
@@ -1,81 +1,94 @@
 from django.db import models
-from django.db import models
-from django.db.models import Q
-from django.utils.text import slugify
-from django.utils.encoding import smart_unicode, smart_str
-from tinymce import models as tinymce_models
+import tinymce
 from bs4 import BeautifulSoup
 import re
+from redactor.fields import RedactorField
+import select2.fields
 
 import os
 import datetime
 
-# Create your models here.
-
-# class Section(models.Model):
-#     name = models.CharField(max_length=255, unique=True)
-
-#     def __unicode__(self):
-#         return self.name
-
-# class Contributor(models.Model):
-#     name = models.CharField(max_length=255, blank=True, null=True, unique=True)
-
-#     def __unicode__(self):
-#       return self.name
-#     def slug(self):
-#       return self.name.replace(" ", "_")
-#     def get_absolute_url(self):
-#      # Can't use .format because name is not always
-#      return '/contributor/' +  str(self.id) + '/' +  self.slug()
-#      # return '/contributor/{0}/{1}'.format(self.id, self.slug())
-
-# class Tag(models.Model):
-#     name = models.CharField(max_length=255)
-#     slug = models.SlugField(max_length=100)
-
-#     def __unicode__(self):
-#         return self.name
-
 class Decade(models.Model):
 	name = models.CharField(max_length=255)
 
-# class ContentQuerySet(models.query.QuerySet):
-#     def published(self):
-#         return self.filter(Q(publishDate__lte=datetime.datetime.now()) | Q(publishDate__isnull=True))
+	def __unicode__(self):
+		return self.name
 
-# class ContentManager(models.Manager):
-#     def get_queryset(self):
-#         return ContentQuerySet(self.model, using=self._db)
 
-#     def published(self):
-#         return self.get_queryset().published()
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=100)
 
-# class Content(models.Model):
-#     objects = ContentManager()
-#     title = models.CharField(max_length=255)
-#     publishDate = models.DateTimeField(default=datetime.datetime.now)
-#     subtitle = models.CharField(max_length=255, blank=True)
-#     slug = models.SlugField(max_length=100, unique=True)
-#     teaser = tinymce_models.HTMLField(blank=True)
-#     body = tinymce_models.HTMLField(blank=True)
+    def __unicode__(self):
+        return self.name
+    def get_absolute_url(self):
+        # Can't use .format because name is not always
+        return '/anthology/tag/' +  str(self.slug)
+        # return '/contributor/{0}/{1}'.format(self.id, self.slug())
 
-#     # Legacy fields; we should probably get rid of this eventually
-#     medium = tinymce_models.HTMLField(blank=True)
-#     size = tinymce_models.HTMLField(blank=True)
-#     statement = tinymce_models.HTMLField(blank=True)
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=100)
+    def __unicode__(self):
+        return self.name
 
-#     issue = models.ForeignKey('Issue')
-#     section = models.ForeignKey('Section', related_name="section")
-#     contributors = models.ManyToManyField(Contributor)
-#     tags = models.ManyToManyField(Tag, blank=True)
 
-#     def __unicode__(self):
-#         return self.title
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=100)
 
-#     def get_absolute_url(self):
-#         return '/content/{0}/'.format(self.id)
+    def __unicode__(self):
+        return self.name
+    def get_absolute_url(self):
+        # Can't use .format because name is not always
+        return '/anthology/contributor/' +  str(self.id)
+        # return '/contributor/{0}/{1}'.format(self.id, self.slug())
 
-#     def description(self):
-#         txt = ' '.join(BeautifulSoup(self.teaser).findAll(text=True))
-#         return txt
+class Images(models.Model):
+    path = models.CharField(max_length=1000, blank=True)
+    caption = models.TextField(max_length=10000, blank=True)
+    slug = models.SlugField(max_length=100)
+    def __unicode__(self):
+        return self.id
+    def get_absolute_url(self):
+        return "/media/" + path
+
+
+class Theme(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length = 100)
+    def __unicode__(self):
+        return self.name
+
+# Create your models here.
+class Content(models.Model):
+    title = models.CharField(max_length=300)
+    slug = models.SlugField(max_length=100)
+    body = RedactorField(
+        verbose_name=u'Text',
+        redactor_options={'lang': 'en', 'focus': 'true'},
+        upload_to='tmp/',
+        allow_file_upload=True,
+        allow_image_upload=True
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    tags = select2.fields.ManyToManyField(Tag)
+    posted = select2.fields.ManyToManyField(Category)
+    authors = select2.fields.ManyToManyField(Author)
+    decade = select2.fields.ForeignKey(Decade)
+    first_image = models.ForeignKey(Images,null=True, blank=True, default = None)
+    def __unicode__(self):
+        return self.title
+    def get_absolute_url(self):
+        return '/anthology/content/'+ self.slug
+    def crop_first_image(self):
+        return self.first_image
+    def teaser(self):
+        txt = re.sub("\{\{.*\}\}","",BeautifulSoup(self.body).text)
+        i = 800
+        while len(txt) > i and txt[i-1] != ".":
+            i += 1
+        if "(function" in txt:
+            if txt.index("(function") < i:
+                i = txt.index("(function") 
+        return txt[:i]
