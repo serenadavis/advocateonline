2065a535f42afe6ff7e95d2be1c1eac9a146fae1
Sameer Mehra <sammysignal@gmail.com>
2016-04-03 17:54:44 -0400

    fixing shouth

--- magazine/migrations/0001_initial.py
+++ magazine/migrations/0001_initial.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.db import migrations, models
 import datetime
 import magazine.models
 import tinymce.models
@@ -27,19 +27,6 @@ class Migration(migrations.Migration):
                 ('size', tinymce.models.HTMLField(blank=True)),
                 ('statement', tinymce.models.HTMLField(blank=True)),
             ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='Article',
-            fields=[
-                ('content_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='magazine.Content')),
-                ('photo', models.ImageField(null=True, upload_to=magazine.models.upload_image_to, blank=True)),
-            ],
-            options={
-            },
-            bases=('magazine.content',),
         ),
         migrations.CreateModel(
             name='Contributor',
@@ -47,9 +34,6 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=255, unique=True, null=True, blank=True)),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Donation',
@@ -68,19 +52,6 @@ class Migration(migrations.Migration):
                 ('customerID', models.CharField(max_length=255)),
                 ('time', models.CharField(max_length=255)),
             ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='Image',
-            fields=[
-                ('content_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='magazine.Content')),
-                ('photo', models.ImageField(upload_to=magazine.models.upload_image_to)),
-            ],
-            options={
-            },
-            bases=('magazine.content',),
         ),
         migrations.CreateModel(
             name='Issue',
@@ -93,9 +64,6 @@ class Migration(migrations.Migration):
                 ('year', models.IntegerField(null=True, blank=True)),
                 ('pub_date', models.DateField()),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Purchase',
@@ -114,9 +82,6 @@ class Migration(migrations.Migration):
                 ('purchases_json', models.CharField(max_length=255)),
                 ('time', models.CharField(max_length=255)),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Section',
@@ -124,9 +89,16 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(unique=True, max_length=255)),
             ],
-            options={
-            },
-            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ShopItem',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=255)),
+                ('description', models.CharField(max_length=255)),
+                ('price', models.DecimalField(max_digits=6, decimal_places=2)),
+                ('image', models.ImageField(null=True, upload_to=magazine.models.get_image_path, blank=True)),
+            ],
         ),
         migrations.CreateModel(
             name='Subscriber',
@@ -145,9 +117,6 @@ class Migration(migrations.Migration):
                 ('subscriptionType', models.CharField(max_length=255, choices=[(b'Three year; US', b'Three year; US'), (b'Two year; US', b'Two year; US'), (b'One year; US', b'One year; US'), (b'Three year; non-US', b'Three year; non-US'), (b'Two year; non-US', b'Two year; non-US'), (b'One year; non-US', b'One year; non-US')])),
                 ('time', models.CharField(max_length=255)),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Tag',
@@ -156,32 +125,41 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=255)),
                 ('slug', models.SlugField(max_length=100)),
             ],
-            options={
-            },
-            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('content_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='magazine.Content')),
+                ('photo', models.ImageField(null=True, upload_to=magazine.models.upload_image_to, blank=True)),
+            ],
+            bases=('magazine.content',),
+        ),
+        migrations.CreateModel(
+            name='Image',
+            fields=[
+                ('content_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='magazine.Content')),
+                ('photo', models.ImageField(upload_to=magazine.models.upload_image_to)),
+            ],
+            bases=('magazine.content',),
         ),
         migrations.AddField(
             model_name='content',
             name='contributors',
             field=models.ManyToManyField(to='magazine.Contributor'),
-            preserve_default=True,
         ),
         migrations.AddField(
             model_name='content',
             name='issue',
             field=models.ForeignKey(to='magazine.Issue'),
-            preserve_default=True,
         ),
         migrations.AddField(
             model_name='content',
             name='section',
             field=models.ForeignKey(related_name='section', to='magazine.Section'),
-            preserve_default=True,
         ),
         migrations.AddField(
             model_name='content',
             name='tags',
             field=models.ManyToManyField(to='magazine.Tag', blank=True),
-            preserve_default=True,
         ),
     ]
