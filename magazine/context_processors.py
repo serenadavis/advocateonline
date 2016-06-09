92cc808915ed55d3511e2e1f487480ed9afb587f
Nicholas Hasselmo <nicholashasselmo@college.harvard.edu>
2016-06-09 00:51:18 -0400

    Add blog post titles and authors to autocomplete

--- magazine/context_processors.py
+++ magazine/context_processors.py
@@ -1,4 +1,5 @@
 from .models import Article, Content, Image, Issue, Contributor
+from blog.models import Post, Author
 from django.core import serializers
 import json
 
@@ -6,11 +7,13 @@ def search_typeahead(request):
     author_list = []
     for dict in Contributor.objects.values():
         author_list.append(dict['name'])
-    
+    for dict in Author.objects.values():
+        author_list.append(dict['name'])
+
     title_list = []
     for dict in Article.objects.values():
         title_list.append(dict['title'])
+    for dict in Post.objects.values():
+        title_list.append(dict['title'])
 
     return {'typeahead_authors' : json.dumps(list(author_list)), 'typeahead_titles' : json.dumps(list(title_list))}
-    
-    
