9a49b83abeecd8fb9621c06b094e6851293a5667
Daniel Inge <daninge98@gmail.com>
2018-12-03 01:16:19 -0500

    nop

--- advo/settings.py
+++ advo/settings.py
@@ -246,7 +246,7 @@ AWS_S3_REGION_NAME = 'us-east-1'  # e.g. us-east-2
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
 
-# Tell django-storages the domain to use to refer to static files.
+# Tell  django-storages the domain to use to refer to static files.
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 # Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
