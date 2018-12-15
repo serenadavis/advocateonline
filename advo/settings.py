4cb0b7d6e21efb5c677f5094fb012674bc7b4973
Daniel Inge <daninge98@gmail.com>
2018-12-15 14:31:56 -0500

    fixed typeahead

--- advo/settings.py
+++ advo/settings.py
@@ -112,7 +112,8 @@ TEMPLATES = [
             'django.template.context_processors.request',
             'django.contrib.auth.context_processors.auth',
             'django.contrib.messages.context_processors.messages',
-            "advo.context_processors.admin_media"
+            "advo.context_processors.admin_media",
+            'magazine.context_processors.search_typeahead'
         ],
     },
 },
