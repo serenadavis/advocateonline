4b678df712733a6115081d6a8c4fac70dea9246f
Daniel Inge <daninge98@gmail.com>
2018-12-23 13:53:05 +0000

    Revmoed local elasticsearch

--- advo/search_settings.py
+++ advo/search_settings.py
@@ -1,7 +1,28 @@
+# HAYSTACK_CONNECTIONS = {
+#     'default': {
+#         'ENGINE': 'haystack.backends.elasticsearch2_backend.Elasticsearch2SearchEngine',
+#         'URL': 'http://127.0.0.1:9200/',
+#         'INDEX_NAME': 'haystack',
+#     },
+# }
+
+from django.conf import settings
+from requests_aws4auth import AWS4Auth
+awsauth = AWS4Auth(settings.AWS_ACCESS_KEY_ID,settings.AWS_SECRET_ACCESS_KEY,'us-east-1','es')
+
+
 HAYSTACK_CONNECTIONS = {
     'default': {
-        'ENGINE': 'haystack.backends.elasticsearch2_backend.Elasticsearch2SearchEngine',
-        'URL': 'http://127.0.0.1:9200/',
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'https://search-advo-search-tdbbozoee2qmgiowsicqdjyfze.us-east-1.es.amazonaws.com',
         'INDEX_NAME': 'haystack',
+        'KWARGS': {
+            'port': 443,
+            'http_auth': awsauth,
+            'use_ssl': True,
+            'verify_certs': True,
+            'connection_class': elasticsearch.RequestsHttpConnection
+        },
+
     },
 }
